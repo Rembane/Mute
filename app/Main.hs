ae0c2755af1e6675075fae13b948cfd1d76072a9
Andreas Ekeroot <andeke@gmail.com>
2018-02-21 13:01:44 +0100

    This is a working version of mute.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
-module Main where
+import System.Environment (getArgs)
+import System.Process (callProcess)
 
-import Lib
+sendNotification :: String -> IO ()
+sendNotification = callProcess "notify-send" . pure
+
+muteIt :: String -> IO ()
+muteIt "y" = callProcess "pactl" ["set-sink-mute", "0", "1"]
+             >> sendNotification "I R Quiet!"
+muteIt "n" = callProcess "pactl" ["set-sink-mute", "0", "0"]
+             >> sendNotification "Let there be sound!"
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= (muteIt . head)
